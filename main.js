4c444cb95719b668be94472768cd76d5c0f03e92
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-18 15:51:41 -0500

    Create append method

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const LinkedList = () => {
     let head = null;
     let length = 0;
 
+    const append = val => {
+        const node = Node(val)
+        length++
+        if (head === null) return (head = node)
+
+        let current = head;
+        while (current.nextNode !== null) {
+            current = current.nextNode
+        }
+        current.nextNode = node
+    }
 }
 
 //Create Node factory containing a value function and a link to the nextNode, set both as null by default.
